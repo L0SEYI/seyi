c0c44d5fa58e1af7701283ecd9f565841d099c8b
1cyberlord <awhansuoluwaseyi@gmail.com>
2025-04-16 16:28:44 +0000

    Implement search functionality with Search component and state management

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Search from './components/Search';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState(''); 
+
   return (
     <main>
     <div className="pattern"/>
     <div className="wrapper" >
     <header>
+      <img src="./hero.png" alt="hero Banner" />
       <h1>Find <span className="text-gradient"> Movies</span> You'll Enjoy without the Hassle </h1>
     </header>
 
-    <p>Search</p>
+   <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+  <h1 className='text-white'>{searchTerm}</h1>
+  
   </div>
   </main>
   )
